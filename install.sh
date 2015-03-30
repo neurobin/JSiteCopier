8a8aed5e8ab48dd680c7effd9eb74693d82039a9
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-03-30 13:03:08 +0600

    Final

---
+++ install.sh
@@ -0,0 +1,13 @@
+BASEDIR=`dirname "${0}"`
+cd "$BASEDIR"
+sudo chmod -R 755 *
+sudo rm -rf /usr/local/JSiteCopier
+sudo mkdir -p /usr/local/JSiteCopier
+sudo cp -R . /usr/local/JSiteCopier
+sudo chmod -R 755 /usr/local/JSiteCopier
+sudo cp jsitecopy /usr/bin
+sudo -s <<EOF
+echo -e "[Desktop Entry]\nName=JSiteCopier\nType=Application\nExec=jsitecopy\nTerminal=true\nIcon=Icon=/usr/local/JSiteCopier/jsitecopy.png\nCategories=Network;Utility;\nComment=Website Copier" > /usr/share/applications/jsitecopy.desktop
+EOF
+echo ".......Install complete!"
+echo ".......See the readme file provided with this software for instructions to use it......"
